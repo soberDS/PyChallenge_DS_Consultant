be9da0728445bcdf7bb4ac52f7d2c81715b9e8cc
soberDS <35286347+soberDS@users.noreply.github.com>
2018-01-10 01:51:28 -0500

    Add files via upload

---
+++ challenge_1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 01:39:41 2018
+@author: Tao Yang
+
+Question 1:
+*** Please don't use numpy or other lib first *** 
+*** If you use too much time, use numpy and/or scipy ***
+
+Haha, we always start to create a function do such thing, zzz...
+     
+Give a random muti-dimention list(or so-called n-by-m matrix), 
+and A(can be any given number), 
+[
+  [A, 5, 3, 4, 9],
+  [8, A, 1, 3, 2],
+  [4, 5, A, 7, 10],
+  [8, 2, 4, A, 13],
+  [7, 4, 1, 2, 11],
+]
+
+To extract hidden pattern  [[ A,      rand_number],
+                            [number,       A      ]]        
+By couting how many of them occured and finding the index-positions. 
+The hidden pattern matrix can be any size,but you can start with 2-by-2 matrix 
+
+"""
+def find_pattern_from(raw_matrix, pattern_matrix):
+    pass
